1b976e49f8c7c330aa624fc63f7374fe740c1a0e
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 06:55:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0,1);
-  return array;
+  var newArray = array.slice(0,1);
+  return newArray;
 }
 
 function removeElementFromEndOfArray(array){

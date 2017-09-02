69eeb66283b00c7ea5c586ab45a50c3c16dbb87c
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 06:37:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,18 @@
-var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+var chocolateBars = [snickers, hundred grand, kitkat, skittles];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array];
+  array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}

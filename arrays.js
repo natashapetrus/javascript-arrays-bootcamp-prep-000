27c0741c9d32397f756c120e55d591ad9954e06c
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 06:46:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,29 +1,29 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array];
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element){
-  [...array, element];
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
+  return array.push(element);
 }
 
 function accessElementInArray(array, index){
-  array[index];
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift();
+  return array.shift();
 }
 
 function removeElementFromEndOfArray(array){
-  array.pop()
+  return array.pop()
 }
